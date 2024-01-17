b9df133479208392632b9c6831d0ca3714b62580
ALESSANDRO FREGONESE <alefrego@hotmail.com>
2024-01-17 21:56:43 +0000

    Remove unused adapters

--- TranslationService.Adapters.Http/ITranslateAdapter.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TranslationService.Adapters.Http;
-
-public interface ITranslateAdapter
-{
-    Task<string> TranslateAsync(string originalText, int originalLanguage, int targetLanguage);
-}
